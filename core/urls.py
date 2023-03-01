e225febdf472a614f6c7fdc7f6177252192e2566
djumanov <diyorbekjumanov1120@gmail.com>
2023-03-01 17:43:34 +0500

    response attribute

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from django.http import HttpRequest
 
 def home(request: HttpRequest):
-    print(request.method)
-    print(request.path)
-    print(request.content_type)
+    data = request.GET
+    print(data['a'])
     pass
 
 
