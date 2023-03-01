42270366c3cb51473c8c1578161584481f3006fa
djumanov <diyorbekjumanov1120@gmail.com>
2023-03-01 18:19:29 +0500

    app and view

---
+++ api/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpRequest, HttpResponse, JsonResponse
+
+def home(request: HttpRequest):
+    # get query agruments
+    a = request.GET.get('a', 0)
+    b = request.GET.get('b', 0)
+
+    # response data
+    response = {
+        "result": int(a) + int(b)
+    }
+
+    return JsonResponse(response)
--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,5 @@
 from django.urls import path
-from django.http import HttpRequest, HttpResponse, JsonResponse
-
-def home(request: HttpRequest):
-    # get query agruments
-    a = request.GET.get('a', 0)
-    b = request.GET.get('b', 0)
-
-    # response data
-    response = {
-        "result": int(a) + int(b)
-    }
-
-    return JsonResponse(response)
+from api.views import home
 
 
 urlpatterns = [
