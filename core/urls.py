30c77fbb1bf9c516ac69f3e58908c26014fa51da
djumanov <diyorbekjumanov1120@gmail.com>
2023-03-01 18:11:51 +0500

    sum

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 from django.http import HttpRequest, HttpResponse, JsonResponse
 
 def home(request: HttpRequest):
+    # get query agruments
+    a = request.GET.get('a', 0)
+    b = request.GET.get('b', 0)
 
-    return JsonResponse({})
+    # response data
+    response = {
+        "result": int(a) + int(b)
+    }
+
+    return JsonResponse(response)
 
 
 urlpatterns = [
